23e24af5ff21b7bbe17cb81b5a918bcf7eff0401
Aditya Sawant <60505090+AdityaSawant21@users.noreply.github.com>
2021-08-22 18:35:17 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const apiURL    = 'https://restcountries.eu/rest/v2/alpha/',
-countries       = ['dza', 'alb', 'ala', 'afg', 'isl', 'bra', 'usa', 'deu', 'ind'],
+countries       = ['dza', 'alb', 'ala', 'afg', 'isl', 'bra', 'usa', 'ind'],
 
 items_Container = document.querySelector('.content__items'),
 selecFilter     = document.querySelector('select'),
