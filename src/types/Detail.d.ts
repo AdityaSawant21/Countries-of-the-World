c5c69be0e240befe6390bab6010031f6f34258ce
Aditya Sawant <60505090+AdityaSawant21@users.noreply.github.com>
2021-08-22 16:31:45 +0530

    Delete src directory

--- src/types/Detail.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Detail {
-  title: string
-  value: string | number
-}
--- src/types/country.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Country {
-  name: string
-  flag: string
-  population: number
-  region: string
-  capital: string
-  alpha3Code: string
-  [extra]: string | number | object | Array
-}
